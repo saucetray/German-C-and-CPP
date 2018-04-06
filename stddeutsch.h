dc09ceb3bd8164d3caf6668064d99c179aea5eae
Saucetray <justinsostreofficial@gmail.com>
2018-04-06 14:49:57 -0400

    stddeutsch.h

---
+++ stddeutsch.h
@@ -0,0 +1,26 @@
+// deutsch c
+
+#include <stdlib.h>
+
+#define stdlib.h stddeutsch.h
+#define include umfassen
+#define ob if
+#define wahr 0
+#define falsch 1
+#define drucken(fmt, ...) printf(fmt, ##__VA_ARGS__)
+
+#define während while
+#define machen do
+
+#define große_t size_t
+#define EXIT_FAILURE SCHEITERN_AUSFAHRT
+#define EXIT_SUCCESS ERFOLG_AUSFAHRT
+#define RAND_MAX ZUFÄLLIG_MAX
+
+// FUNCTIONS - FUNKTIONEN
+#define azus(zeich) atol(zeich)
+#define azug(zeich) atoi(zeich)
+#define zeichzud(zeich, zeig) strod(zeich, zeig)
+//#define strol
+
+
